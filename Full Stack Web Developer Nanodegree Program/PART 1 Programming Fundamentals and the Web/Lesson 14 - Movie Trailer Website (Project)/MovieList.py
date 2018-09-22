739d918ae578edb297db65c7719b10810a71da5c
Josh MacDonnell <macdonnell.josh@gmail.com>
2018-09-22 01:38:13 -0400

    Updated CSS. Found a section of script that didnt work so I commented it out. Comment says it dealt with animate in the movies when the page loads.
    Also added plot_summary to movie class

--- Full Stack Web Developer Nanodegree Program/PART 1 Programming Fundamentals and the Web/Lesson 14 - Movie Trailer Website (Project)/MovieList.py
+++ Full Stack Web Developer Nanodegree Program/PART 1 Programming Fundamentals and the Web/Lesson 14 - Movie Trailer Website (Project)/MovieList.py
@@ -7,6 +7,8 @@ the_avengers = media.Movie(
     "https://m.media-amazon.com/images/M/"
     "MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5Z"
     "GI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+    "Earth's mightiest heroes must come together and learn to fight as a team "
+    "if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
     "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
 the_avengers2 = media.Movie(
@@ -14,6 +16,9 @@ the_avengers2 = media.Movie(
     "https://m.media-amazon.com/images/M/MV5BMTM4O"
     "GJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg"
     "@._V1_SY1000_SX675_AL_.jpg",
+    "When Tony Stark and Bruce Banner try to jump-start a dormant peacekeeping "
+    "program called Ultron, things go horribly wrong and it's up to Earth's mightiest "
+    "heroes to stop the villainous Ultron from enacting his terrible plan.",
     "https://www.youtube.com/watch?v=tmeOjFno6Do")
 
 the_avengers3 = media.Movie(
@@ -21,6 +26,8 @@ the_avengers3 = media.Movie(
     "https://m.media-amazon.com/images/M/"
     "MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM"
     "@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    "The Avengers and their allies must be willing to sacrifice all in an attempt to "
+    "defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
     "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
 
 # Storing created movies into an array.
--- Full Stack Web Developer Nanodegree Program/PART 1 Programming Fundamentals and the Web/Lesson 14 - Movie Trailer Website (Project)/media.py
+++ Full Stack Web Developer Nanodegree Program/PART 1 Programming Fundamentals and the Web/Lesson 14 - Movie Trailer Website (Project)/media.py
@@ -5,7 +5,8 @@ import webbrowser
 class Movie():
     # Called upon initialization.
     # Gathers the information required for our movie object.
-    def __init__(self, movie_tile, poster_image, trailer_youtube):
+    def __init__(self, movie_tile, poster_image, plot_summary, trailer_youtube):
         self.title = movie_tile
         self.poster_image_url = poster_image
+        self.plot = plot_summary
         self.trailer_youtube_url = trailer_youtube
